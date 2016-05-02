36e22a788dddb28c0e1d3b1f30aed1d4164052e5
Evan Lok <elok45@gmail.com>
2016-05-02 15:11:44 -0700

    Fix theme settings component

--- client/app/theme_settings/theme_settings.module.js
+++ client/app/theme_settings/theme_settings.module.js
@@ -1,7 +1,7 @@
 var themeSettings = angular.module('adsee.uploader', []);
 
 themeSettings
-  .component('uploader', require('./theme_settings.component'))
+  .component('themeSettings', require('./theme_settings.component'))
   .component('songPlayer', require('./song_player.component.js'))
   .service('songsService', require('./songs.service'))
   .service('fontsService', require('./fonts.service'));
