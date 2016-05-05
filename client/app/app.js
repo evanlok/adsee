c66ef353933d6ad5629a48f53e42974a2210a8ea
Evan Lok <elok45@gmail.com>
2016-05-05 16:05:09 -0700

    Set SSL on a controller basis

--- client/app/app.js
+++ client/app/app.js
@@ -28,15 +28,17 @@ angular.module('adsee', [
   require('./summary/summary.module'),
   require('./media/media.module')
 ]).config(config)
-  .run(function /*@ngInject*/($rootScope, $window, $state, sslEnabled) {
+  .run(function /*@ngInject*/($rootScope, $window, $state, SSL_ENABLED) {
     // Redirect preview to non-ssl url so that streaming works correctly
     $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams, options) {
-      if (sslEnabled) {
+      if (SSL_ENABLED) {
         var hostWithPort = $window.location.hostname + ($window.location.port ? ':' + $window.location.port : '');
         var path = $state.href(toState.name, toParams);
 
-        if (toState.name === 'preview' && $window.location.protocol !== 'http:') {
-          $window.location = 'http://' + hostWithPort + path;
+        if (toState.name === 'preview') {
+          if ($window.location.protocol !== 'http:') {
+            $window.location = 'http://' + hostWithPort + path;
+          }
         } else if ($window.location.protocol !== 'https:') {
           $window.location = 'https://' + hostWithPort + path;
         }
