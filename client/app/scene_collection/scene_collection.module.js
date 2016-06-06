95961509a8582b4157afa2196c4cecc74a57c58d
Evan Lok <elok45@gmail.com>
2016-06-06 15:31:31 -0700

    Fix module

--- client/app/scene_collection/scene_collection.module.js
+++ client/app/scene_collection/scene_collection.module.js
@@ -2,6 +2,5 @@ var sceneCollection = angular.module('adsee.sceneCollection', []);
 
 sceneCollection
   .directive('newSceneCollectionLink', require('./new_scene_collection_link.directive'))
-  .service('stepNavigator', require('./step_navigator.service'));
 
 module.exports = sceneCollection.name;
