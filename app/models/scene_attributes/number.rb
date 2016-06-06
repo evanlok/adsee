081c93cc1dbdd4087c0c36e370fab474d201c1d0
Evan Lok <elok45@gmail.com>
2016-06-06 16:14:34 -0700

    Allow number attributes to be null

--- app/models/scene_attributes/number.rb
+++ app/models/scene_attributes/number.rb
@@ -1,10 +1,11 @@
 module SceneAttributes
   class Number < SceneAttribute
     # Validations
-    validates :value, numericality: true
+    validates :value, numericality: true, allow_nil: true
 
     def value
       number = self[:value]
+      return unless number
       raise ParseError, "Cannot parse number from value: #{self[:value]}" unless number =~ /\A\d*\.?\d*\z/
       number =~ /\./ ? number.to_f : number.to_i
     end
