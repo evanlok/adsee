b102183d6cc2433623affda5538ca65a7da54d2a
Evan Lok <elok45@gmail.com>
2016-06-09 16:52:40 -0700

    Display video duration on scene editor

--- client/app/shared/shared.module.js
+++ client/app/shared/shared.module.js
@@ -2,7 +2,7 @@ var shared = angular.module('adsee.shared', []);
 
 shared
   .component('videoPlayer', require('./video_player.component'))
-  .directive('videoOverlayLink', require('./video_overlay_link.directive'));
-
+  .directive('videoOverlayLink', require('./video_overlay_link.directive'))
+  .filter('secondsToTime', require('./seconds_to_time.filter'));
 
 module.exports = shared.name;
