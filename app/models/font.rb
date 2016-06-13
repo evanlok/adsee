e7f599f0753459e81c3c2c7657e30d3ac7771579
Evan Lok <elok45@gmail.com>
2016-06-13 16:38:10 -0700

    Add image attachments to fonts

--- app/models/font.rb
+++ app/models/font.rb
@@ -1,4 +1,6 @@
 class Font < ActiveRecord::Base
+  mount_uploader :image, FileUploader
+
   # Associations
   has_many :themes, dependent: :nullify
   has_many :scene_collections, dependent: :nullify
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160608184302) do
+ActiveRecord::Schema.define(version: 20160613232438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20160608184302) do
     t.text     "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "image"
   end
 
   create_table "images", force: :cascade do |t|
