5f814efb1eb2dab7cd4bc7d7863394297a405ab4
Evan Lok <elok45@gmail.com>
2016-06-28 15:44:50 -0700

    Update gems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
     respond_to do |format|
       format.html do
         flash[:alert] = error
-        redirect_to(request.referrer || root_path)
+        redirect_to(request.referer || root_path)
       end
 
       format.json do
