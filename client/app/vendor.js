30b8da1f98963107e64f30524b10910b19b94943
Evan Lok <elok45@gmail.com>
2016-08-22 11:19:34 -0700

    Move vendor js requires to vendor.js file

--- client/app/vendor.js
+++ client/app/vendor.js
@@ -1,1 +1,42 @@
+// Vendor
+require('jquery');
+require('jquery-ujs');
+require('bootstrap-sass');
+require('lodash');
+require('angular');
+require('angular-resource');
+require('angular-animate');
+require('ng-fx');
+require('./../js/angular-ui-router');
+require('angular-ui-bootstrap');
+require('angular-tree-control');
+require('angular-easyfb');
+require('angularjs-slider');
+require('angular-xeditable');
+require('ui-select');
+require('angularjs-color-picker/dist/angularjs-color-picker');
+require('angular-timer');
+require('angular-scroll');
+require('angular-bootstrap-show-errors');
+require('tinycolor2');
+require('select2');
+require('sortablejs/ng-sortable');
+require('filepicker-js');
+require('video.js');
+require('howler');
+require('format-as-currency');
+require('moment');
+require('humanize-duration');
+require('getusermedia');
+require('scriptjs');
+require('slick-carousel');
+require('node-uuid');
+require('bowser');
+require('toastr');
+require('js-sha256');
+
+// Lib
+require('./../js/recorder.js');
+
+// CSS
 require('./../style/vendor.scss');
